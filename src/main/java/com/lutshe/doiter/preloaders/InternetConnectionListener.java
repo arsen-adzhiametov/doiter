ebf620cd65226c4d8607eb34c9eac7bc5cdaaaa3
Kalpazan <kalpazang@gmail.com>
2013-09-12 20:26:08 +0300

    Connection Listener behaviour depends on connection type

--- src/main/java/com/lutshe/doiter/preloaders/InternetConnectionListener.java
+++ src/main/java/com/lutshe/doiter/preloaders/InternetConnectionListener.java
@@ -7,35 +7,48 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.Log;
 import com.commonsware.cwac.wakeful.WakefulIntentService;
+import org.joda.time.DateTime;
+
+import static android.content.Context.MODE_PRIVATE;
+import static android.net.ConnectivityManager.TYPE_WIFI;
 
 /**
  * Created by Arsen Adzhiametov on 9/09/13.
  */
 public class InternetConnectionListener extends BroadcastReceiver {
 
-	private static final String TAG = InternetConnectionListener.class.getName();
+    private static final String TAG = InternetConnectionListener.class.getName();
+    private static final int MAX_DAYS_PERIOD_WITHOUT_UPDATE = 5;
+    private Context context;
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		Log.i(TAG, "Action: " + intent.getAction());
-		
-    	if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
-		    NetworkInfo info = intent.getParcelableExtra(ConnectivityManager.EXTRA_NETWORK_INFO);
-		    
-		    if (info == null) return;
-		    
-		    String typeName = info.getTypeName();
-		    String subtypeName = info.getSubtypeName();
-		    boolean available = info.isAvailable();
-		    
-		    Log.i(TAG, "Network Type: " + typeName
-				+ ", subtype: " + subtypeName
-				+ ", available: " + available);	
-		    
-		    if (available) {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        this.context = context;
+        Log.i(TAG, "Action: " + intent.getAction());
+        if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+            NetworkInfo info = intent.getParcelableExtra(ConnectivityManager.EXTRA_NETWORK_INFO);
+            if (info == null) return;
+
+            boolean available = info.isAvailable();
+            Log.i(TAG, "Network Type: " + info.getTypeName()
+                    + ", subtype: " + info.getSubtypeName()
+                    + ", available: " + available);
+
+            if (available && (info.getType() == TYPE_WIFI || isLongPeriodWithoutUpdate())) {
                 WakefulIntentService.sendWakefulWork(context, LoaderService_.class);
-		    }
-    	}
-	}
+            }
+        }
+    }
+
+    private boolean isLongPeriodWithoutUpdate() {
+        DateTime lastSyncTime = new DateTime(getLastSyncTime());
+        return lastSyncTime.plusDays(MAX_DAYS_PERIOD_WITHOUT_UPDATE).isBeforeNow();
+    }
+
+    private long getLastSyncTime() {
+        long lastGoalSyncTime = context.getSharedPreferences("loaders", MODE_PRIVATE).getLong(MessagesLoader.class + "lastCall", 0);
+        long lastMessageSyncTime = context.getSharedPreferences("loaders", MODE_PRIVATE).getLong(GoalsLoader.class + "lastCall", 0);
+        return lastGoalSyncTime > lastMessageSyncTime ? lastMessageSyncTime : lastGoalSyncTime;
+    }
 
 }
